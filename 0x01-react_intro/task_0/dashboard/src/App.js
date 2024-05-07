8beebd2aa2399f15bc9b52c2e055794a5e508849
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-05-07 18:06:07 +0100

    task_0 aded

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,24 @@
+import './App.css'
+
+import holberton_logo from './holberton_logo.jpg'
+
+export default function App() {
+  return (
+    <>
+    <div className="App-header">
+      <img src={holberton_logo} alt="Logo"></img>
+      <h1>School dashboard</h1>
+    </div>
+
+    <div className="App-body">
+      <p>Login to access the full dashboard</p>
+    </div>
+
+    <div className="App-footer">
+      <p>Copyright 2020 - holberton School</p>
+    </div>
+
+
+    </>
+  );
+}
