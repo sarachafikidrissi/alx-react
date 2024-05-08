2fd5b4d4415b7da4050d85db4b4ddd57968bea8a
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-05-08 17:05:55 +0100

    task 5 updates

--- 0x01-react_intro/task_5/dashboard/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'production',
   entry: './src/index.js',
+  performance: {
+    maxAssetSize: 1000000,
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
@@ -14,7 +19,26 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+            "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+        }
+        ]
       }
+      
+      
     ]
   },
   resolve: {
