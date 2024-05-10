0be5e9da750ad4afbfc2b6833b8356073dae9c18
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-05-10 03:34:42 +0100

    task 1

---
+++ 0x02-react_props/task_1/dashboard/src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { getFullYear, getFooterCopy } from "../utils/utils";
+import "./Footer.css";
+
+
+function Footer() {
+    return (
+        <>
+            <div className="App-footer">
+            Copyright {getFullYear()} - {getFooterCopy()}
+            </div>
+        </>
+    );
+}
+
+export default Footer;
