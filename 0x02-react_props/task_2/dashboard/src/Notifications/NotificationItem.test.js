d91d01a7cc44e23c15ad044b7417283bd8b76cfc
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-05-12 19:08:12 +0100

    task_2 Split the Notifications component

---
+++ 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { shallow } from "enzyme";
+import NotificationItem from "./NotificationItem";
+
+describe("NotificationItem component test", () => {
+    it("renders without crashing", () => {
+        const component = shallow( <NotificationItem />);
+        expect(component.exists()).toBe(true);
+    });
+
+    it('render correct html from type="default" value="tes" props', () => {
+        const wrapper = shallow(<NotificationItem />);
+
+        wrapper.setProps({ type: "default", value: "test" });
+        expect(wrapper.html()).toEqual('<li data-notification-type="default">test</li>');
+    });
+
+    it('renders correct html from  html="<u>test</u>" props', () => {
+        const wrapper = shallow(<NotificationItem />);
+    
+        wrapper.setProps({ html: "<u>test</u>" });
+        expect(wrapper.html()).toEqual('<li data-urgent="true"><u>test</u></li>');
+      });
+});
